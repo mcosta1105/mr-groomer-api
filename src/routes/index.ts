ca43bb63919b931b58cb01fbb5cd4818159440b4
Maycon Costa <costa.maycon@live.com>
2021-07-29 00:04:41 +1000

    Create user service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import bookingsRouter from './bookings.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/bookings', bookingsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
