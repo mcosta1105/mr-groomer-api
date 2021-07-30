645a2a834290f8a34e9e6857af0b6d98ce561854
Maycon Costa <costa.maycon@live.com>
2021-07-30 23:37:31 +1000

    Add jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -8,8 +9,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -24,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Email or password not valid.');
     }
 
+    const token = sign({}, 'dfa1aaf81be60ec6646188bc02cdbe16e06e9aa6', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
