8baea0f5c37e2b4a8d604740966a320f0fc69151
Maycon Costa <costa.maycon@live.com>
2021-08-02 23:43:37 +1000

    Add upload avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+
 import isAuth from '../middlewares/auth';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -24,8 +30,13 @@ usersRouter.post('/', async (req, res) => {
   }
 });
 
-usersRouter.patch('/avatar', isAuth, async (req, res) => {
-  return res.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  isAuth,
+  upload.single('avatar'),
+  async (req, res) => {
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
